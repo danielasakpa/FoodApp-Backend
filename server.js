6c9c71555b916a75f9ad1fe5277afbb9e30a8d5f
danielasakpa <danielasakpa@gmail.com>
2023-11-20 10:50:09 +0100

    Added a origin url for cors in server

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.use(compress());
 // secure apps by setting various HTTP headers
 app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
-app.use(cors());
+app.use(cors({
+  origin: "https://foodapp-w2.netlify.app",
+}));
 
 // mount routes
 app.use("/", userRoutes);
